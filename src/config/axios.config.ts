704d744901e481ad4ae9e8142667f542440b4475
Mohamed <gemy46349@gmail.com>
2025-02-24 11:13:08 +0200

    Finish Dashboard

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:3000/api";
+const baseURL = "https://kidskiosk-api.vercel.app/api";
 
 export const axiosInstance = axios.create({
   baseURL,
